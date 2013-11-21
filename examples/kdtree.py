3384b49db9c73d6a6e9aea3fd1453d8422dab483
Christoph Klein <christoph.t.klein@me.com>
2013-11-21 13:31:28 -0600

    reverted kdtree.py

--- examples/kdtree.py
+++ examples/kdtree.py
@@ -1,3 +1,5 @@
+import pdb
+
 from groupy.gbb import *
 from groupy.mdio import *
 from groupy.visualization import *
@@ -5,7 +7,7 @@ from groupy.system import *
 from groupy.general import *
 
 
-'''centers = list()
+centers = list()
 mpcs = list()
 # create a bunch of chains in space
 for i in range(4):
@@ -19,23 +21,19 @@ for i in range(4):
             mpc.calc_com()
             centers.append(mpc.com)
             mpcs.append(mpc)
-'''
-lt = 'unwrapped.lammptrj'
 
-with open(lt, 'r') as trj:
-	while True:
-		try:
-			xyz, types, step, box = read_frame_lammpstrj(trj)
-		except:
-			print 'End of file'
-			break
+dims=np.array([[0, 100], [0, 100], [0, 100]])
 
-		CER_coords = xyz[0,72*49]
-		CER_molecules = np.split(CER_coords, range(0, 72*49, 49))
-		CHOL_coords = 
+# show the chains
+sys = System()
+sys.append_gbbs(mpcs)
+splat(sys.xyz, sys.types, dims=dims)
 
+# show the centers of mass and highlight the one we want to look at
+centers = np.array(centers)
+splat(centers, highlight=25, dims=dims)
 
-
+# show 6 closest neighbors to com of chain # 25
 neighbors = get_points_in_range(centers, centers[25], 200, 7)
 splat(centers[neighbors], highlight=0, dims=dims)
 
