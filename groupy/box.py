4247ae0bf0378c1172362949ae93c04e7869ff24
Tim Moore <tcmoore3@gmail.com>
2014-12-20 22:56:05 -0600

    bounding box for Box

--- groupy/box.py
+++ groupy/box.py
@@ -39,3 +39,19 @@ class Box():
             self.maxs = self.dims[:, 1]
             self.length = self.maxs - self.mins
             self.volume = self.length[0] * self.length[1] * self.length[2]
+
+    def bounding_box(self, gbbs):
+        mins = np.array([np.inf, np.inf, np.inf])
+        maxs = np.array([-np.inf, -np.inf, -np.inf])
+        for lipid in gbbs:
+            mins = np.array([min(mins[0], np.amin(lipid.xyz[:, 0])),
+                min(mins[1], np.amin(lipid.xyz[:, 1])),
+                min(mins[2], np.amin(lipid.xyz[:, 2]))])
+            maxs = np.array([max(maxs[0], np.amax(lipid.xyz[:, 0])),
+                max(maxs[1], np.amax(lipid.xyz[:, 1])),
+                max(maxs[2], np.amax(lipid.xyz[:, 2]))])
+        self.mins = mins
+        self.maxs = maxs
+        self.lengths = np.array([maxs[0] - mins[0],
+            maxs[1] - mins[1],
+            maxs[2] - mins[2]])
